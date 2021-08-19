f91fe748a0be0d5bf34618f4941d99adc5a89243
hasanbolatt <87964820+hasanbolatt@users.noreply.github.com>
2021-08-19 15:36:36 +0300

    duzenlenmiş hali

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const express = require('express'); 
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 app.use(express.static('public'));
+app.set("view engine","ejs");
 
-app.get('/',(req,res)=>{
+/*app.get('/',(req,res)=>{
     const blog = {
         id: 1,
         title: "Blog title",
         description: "Blog description"
       }
       res.send(blog);
-})
+})*/
 
 app.get('/',(req,res)=>
 {
-    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+    res.render('index');
 })
 
-const port = 3000;
+app.get('/about',(req,res)=>
+{
+    res.render('about');
+})
+
+app.get('/add_post',(req,res)=>
+{
+    res.render('add_post');
+})
+
+const port = 4000;
 
 app.listen(port,()=>
 {
-    console.log('Sunucu ${port} de çalışmaya başladı');
+    //parantezi `` olarak aç..
+    console.log(`Sunucu ${port} de çalışmaya başladı`);
 })
